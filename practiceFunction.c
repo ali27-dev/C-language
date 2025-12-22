c8f1dfb45c069314810ae22b98feff3c2aa2da95
ali27-dev <khanali3229@gmail.com>
2025-12-22 11:35:37 +0500

    little bit practice on funx and move to recursion and exloring it.

--- practiceFunction.c
+++ practiceFunction.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 // declaration/prototype
 /*
 void printHello();
@@ -6,15 +7,31 @@ void goodBye();
 void namaste();
 void banjour();
 int sum(int a, int b);
-*/
 void numTable(int n);
+*/
+
+float areaSqure(float side);
+float areaCircul(float rad);
+float areaRectangle(float a, float b);
+
+// void print(void *x, int type)
+// {
+//   if (type == 1)
+//   {
+//     printf("Integer: %d\n", *((int *)x));
+//   }
+//   else if (type == 2)
+//   {
+//     printf("Float: %.2f\n", *((float *)x));
+//   }
+// }
 
 int main()
 {
-  int n;
-  printf("Enter a Number:");
-  scanf("%d", &n);
-  numTable(n);
+  printf("Area of the Squre:%.2f\n", areaSqure(6.7));
+  printf("Area of the Circule:%.2f\n", areaCircul(3.5));
+  printf("Area of the Circule:%.2f\n", areaRectangle(5.9, 3.6));
+
   return 0;
 }
 
@@ -42,7 +59,7 @@ int sum(int a, int b)
 {
   return a * b;
 }
-     */
+
 
 void numTable(int n)
 {
@@ -50,4 +67,18 @@ void numTable(int n)
   {
     printf("%d\n", i * n);
   }
+}
+  */
+float areaSqure(float side)
+{
+  return side * side;
+}
+
+float areaCircul(float rad)
+{
+  return 3.14 * rad * rad;
+}
+float areaRectangle(float a, float b)
+{
+  return a * b;
 }
