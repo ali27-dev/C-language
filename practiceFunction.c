cd97389f8d8e870d1f8f3598c0555811541dbd7f
ali27-dev <khanali3229@gmail.com>
2025-12-22 12:39:24 +0500

    the functiona nd recursion topic and practice is complete,Let's move to pointer topic.

--- practiceFunction.c
+++ practiceFunction.c
@@ -8,11 +8,14 @@ void namaste();
 void banjour();
 int sum(int a, int b);
 void numTable(int n);
-*/
-
 float areaSqure(float side);
 float areaCircul(float rad);
 float areaRectangle(float a, float b);
+int printHW(int count);
+int sum(int n);
+*/
+
+float percentageStu(int sub1, int sub2, int sub3);
 
 // void print(void *x, int type)
 // {
@@ -28,10 +31,12 @@ float areaRectangle(float a, float b);
 
 int main()
 {
-  printf("Area of the Squre:%.2f\n", areaSqure(6.7));
-  printf("Area of the Circule:%.2f\n", areaCircul(3.5));
-  printf("Area of the Circule:%.2f\n", areaRectangle(5.9, 3.6));
-
+  // printf("Area of the Squre:%.2f\n", areaSqure(6.7));
+  // printf("Area of the Circule:%.2f\n", areaCircul(3.5));
+  // printf("Area of the Circule:%.2f\n", areaRectangle(5.9, 3.6));
+  // printf("Sum is: %d", sum(5));
+  // printHW(5);
+  printf("Total percentage is :%f", percentageStu(78, 84, 78));
   return 0;
 }
 
@@ -68,7 +73,6 @@ void numTable(int n)
     printf("%d\n", i * n);
   }
 }
-  */
 float areaSqure(float side)
 {
   return side * side;
@@ -81,4 +85,30 @@ float areaCircul(float rad)
 float areaRectangle(float a, float b)
 {
   return a * b;
+}
+int printHW(int count)
+{
+  if (count == 0)
+  {
+    return 0;
+  }
+
+  printf("Hello World\n");
+  printHW(count - 1);
+}
+int sum(int n)
+{
+  if (n == 1)
+  {
+    return 1;
+  }
+  int sumNm1 = sum(n - 1);
+  int sumN = sumNm1 + n;
+  return sumN;
+}
+  */
+
+float percentageStu(int sub1, int sub2, int sub3)
+{
+  return (sub1 + sub2 + sub3) / 3;
 }
