f4be02cab224e33b1820c2950c9ec21284274c1e
ali27-dev <khanali3229@gmail.com>
2025-12-25 21:51:15 +0500

    Practice on pointer.

--- practicePointer.c
+++ practicePointer.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// void _swap(int *a, int *b);
+void printAdd(int *n);
+/*
 int main()
 {
   int x;
@@ -18,5 +21,39 @@ int main()
   (*ptr)++;
   printf("x = %d\n", x);
   printf("*ptr = %d\n", *ptr);
+
   return 0;
+}
+
+
+int main()
+{
+  int x = 4;
+  int y = 7;
+  _swap(&x, &y);
+  printf("x = %d & y = %d\n", x, y);
+  return 0;
+}
+
+void _swap(int *a, int *b)
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+
+  printf("a = %d & b = %d\n", *a, *b);
+}
+  */
+
+int main()
+{
+  int n = 4;
+  printAdd(&n);
+  printf("Address 0f n = %u\n", &n);
+  return 0;
+}
+
+void printAdd(int *n)
+{
+  printf("Address 0f n = %u\n", n);
 }
